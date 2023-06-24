c8c298dd36e47263065217495d02a96a77bf22bd
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-06-24 22:10:22 +0530

    Added the expect error for the url argument.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
 import { Redis } from "@upstash/redis";
 
 export const db = new Redis({
+  // @ts-expect-error
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
