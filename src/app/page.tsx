0af3d64475e61fdde2d9c1936d9a9067145821dd
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-06-25 10:34:02 +0530

    Removing the db.set call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import { db } from "@/lib/db";
 import Button from "@/components/ui/Button";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-
   return (
     <main className="">
-      <Button>hello</Button>
+      <Button>Hello</Button>
     </main>
   );
 }
