4ed5253a605a5485fdc91b1a834bca511954e20b
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-06-25 10:29:06 +0530

    Remediated the button text styling issue

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-slate-900 text-white, button-slate-800",
+        default: "bg-slate-900 text-white button-slate-800",
         ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
       },
       size: {
