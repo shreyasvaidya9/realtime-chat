f752a8be4363e18e68dbd217067df76982118319
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-07-01 18:08:22 +0530

    Added the Google Authentication with Next Auth package.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import React, { type ReactNode } from "react";
 
+import Providers from "@/components/Providers";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { FC, useState } from "react";
+import { signIn } from "next-auth/react";
+import { toast } from "react-hot-toast";
+
+import Button from "@/components/ui/Button";
+import Google from "@/components/svg/Google";
+
+interface pageProps {}
+
+const Page: FC<pageProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      // throw new Error("");
+      await signIn("google");
+    } catch (error) {
+      // display error message to user
+      toast.error("Something went wrong with your login");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full flex flex-col items-center max-w-md space-y-8">
+          <div className="flex flex-col items-center gap-8">
+            logo
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <Button
+            isLoading={isLoading}
+            type="button"
+            className="max-w-sm mx-auto w-full"
+            onClick={loginWithGoogle}
+          >
+            {isLoading ? null : <Google />}
+            Google
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
