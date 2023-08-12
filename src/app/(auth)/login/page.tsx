ff96d9b6c433864de6ce282d0a9fae811b2d98b0
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-08-12 16:11:17 +0530

    Added the Logo in the login page.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,6 +6,7 @@ import { toast } from "react-hot-toast";
 
 import Button from "@/components/ui/Button";
 import Google from "@/components/svg/Google";
+import { Icons } from "@/components/Icons";
 
 interface pageProps {}
 
@@ -30,7 +31,7 @@ const Page: FC<pageProps> = ({}) => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            logo
+            <Icons.Logo />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
